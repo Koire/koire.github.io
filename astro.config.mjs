567eb1a85382524c07c3bf0e39be4b75d2fea77e
koire <pikewb@gmail.com>
2023-02-03 10:37:17 +0900

    fix: add import for NetlifyCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import NetlifyCMS from "astro-netlify-cms"
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://koire.github.io',
-    integrations: [
-        NetlifyCMS({
-          config: {
-            backend: {
-              name: 'git-gateway',
-              branch: 'main',
-            },
-            collections: [
-              // Content collections
-            ],
-          },
-        }),
-      ]
-});
+	site: "https://koire.github.io",
+	integrations: [
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "git-gateway",
+					branch: "main",
+				},
+				collections: [
+					{
+						name: "posts",
+						label: "Blog Posts",
+						folder: "src/pages/posts",
+						create: true,
+						delete: true,
+						fields: [
+							{ name: "title", widget: "string", label: "Post Title" },
+							{ name: "body", widget: "markdown", label: "Post Body" },
+						],
+					}
+				],
+			},
+		}),
+	]
+})
