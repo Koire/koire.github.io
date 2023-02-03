c0fd0219849f08390774c1d0797bdc9c8783350d
koire <pikewb@gmail.com>
2023-02-03 10:30:57 +0900

    feat: add netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://koire.github.io',
+    integrations: [
+        NetlifyCMS({
+          config: {
+            backend: {
+              name: 'git-gateway',
+              branch: 'main',
+            },
+            collections: [
+              // Content collections
+            ],
+          },
+        }),
+      ]
 });
